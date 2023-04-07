29f8d0f83591bbbb037ec856e964d2af597c79b3
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-07 11:30:37 +0000

    Create and read database-programmer

---
+++ sql-crud.py
@@ -0,0 +1,119 @@
+from sqlalchemy import (
+    create_engine, Column, Float, ForeignKey, Integer, String
+)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#  Executing the instruction from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+# Create a class-based for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    Id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+# Instead of connecting to the databae directly, we will ask a session
+# Create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# Opens an actual session by calling the session () subclass defined above
+session = Session()
+
+# Creating the database using declarative_base subclass
+base.metadata.create_all(db)
+
+
+# Creating recods on our programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="F",
+    nationality="British",
+    famous_for="First Programmer"
+)
+
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
+pj = Programmer(
+    first_name="PJ",
+    last_name="",
+    gender="M",
+    nationality="Sri Lankan",
+    famous_for="W3"
+)
+
+
+# add each instance of our programmers to our session
+# session.add(ada_lovelace)
+# session.add(alan_turing)
+# session.add(grace_hopper)
+# session.add(margaret_hamilton)
+# session.add(bill_gates)
+# session.add(tim_berners_lee)
+# session.add(pj)
+
+
+
+# commit our session to the database
+# session.commit()
+
+
+# query the database to find all Programmers
+# Q1 - Select all records from the "Artist" table
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.Id,
+        programmer.first_name,
+        programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for, 
+        sep=" | "
+        )
