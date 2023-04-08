9a7903cc6042869e1c5d7fde828d0e5971c81a1a
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-04-08 00:00:22 +0000

    crud challenge

--- sql-favcontry.py
+++ sql-favcontry.py
@@ -18,7 +18,6 @@ class Country(base):
     cap_city = Column(String)
     population = Column(Integer)
     gdp_in_usd_T = Column(Float)
-    presedent: Column(String)
 
 
 # Instead of connecting to the databae directly, we will ask a session
@@ -30,48 +29,50 @@ session = Session()
 # Creating the database using declarative_base subclass
 base.metadata.create_all(db)
 
-# Creating records for country table
-united_states = Country(
-    name="United States",
-    cap_city="Washington D.C.",
-    population=331449281,
-    gdp_in_usd_T=22.67
-)
 
-japan = Country(
-    name="Japan",
-    cap_city="Tokyo",
-    population=126050804,
-    gdp_in_usd_T=5.15
-)
+# # Creating records for country table
+# united_states = Country(
+#     name="United States",
+#     cap_city="Washington D.C.",
+#     population=331449281,
+#     gdp_in_usd_T=22.67
+# )
 
-germany = Country(
-    name="Germany",
-    cap_city="Berlin",
-    population=83149300,
-    gdp_in_usd_T=4.24
-)
+# japan = Country(
+#     name="Japan",
+#     cap_city="Tokyo",
+#     population=126050804,
+#     gdp_in_usd_T=5.15
+# )
 
-brazil = Country(
-    name="Brazil",
-    cap_city="Brasilia",
-    population=214132099,
-    gdp_in_usd_T=2.48
-)
+# germany = Country(
+#     name="Germany",
+#     cap_city="Berlin",
+#     population=83149300,
+#     gdp_in_usd_T=4.24
+# )
 
-egypt = Country(
-    name="Egypt",
-    cap_city="Cairo",
-    population=104124440,
-    gdp_in_usd_T=0.33
-)
+# brazil = Country(
+#     name="Brazil",
+#     cap_city="Brasilia",
+#     population=214132099,
+#     gdp_in_usd_T=2.48
+# )
 
-malaysia = Country(
-    name="Malaysia",
-    cap_city="Kuala Lampur",
-    population=32776194,
-    gdp_in_usd_T=333.24
-)
+# egypt = Country(
+#     name="Egypt",
+#     cap_city="Cairo",
+#     population=104124440,
+#     gdp_in_usd_T=0.33
+# )
+
+# malaysia = Country(
+#     name="Malaysia",
+#     cap_city="Kuala Lampur",
+#     population=32776194,
+#     gdp_in_usd_T=333.24
+# )
+
 
 # add each instance of our programmers to our session
 # session.add(united_states)
@@ -81,12 +82,27 @@ malaysia = Country(
 # session.add(egypt)
 # session.add(malaysia)
 
-# Create and update current hos
-country = session.query(Country).filter_by(name="United States")
-country.presedent = "Joe Biden"
-
 # commit our session to the database
 # session.commit()
+
+# Deleting a single record
+
+dcountry = input("Enter the name of the country you want to delete: ")
+
+country = session.query(Country).filter_by(name=dcountry).first()
+# defensive programing
+if country is not None:
+    print("Country Found! it is", country.name)
+    confirmation = input("Are you sure, you want to delete it? y/n ")
+    if confirmation.lower() == "y":
+        session.delete(country)
+        session.commit()
+        print("Country just now deleted")
+    else: 
+        print("Country not deleted")
+else:
+    print("No country fond")
+
 
 # query the database to find all countries
 countries = session.query(Country)
@@ -97,7 +113,7 @@ for country in countries:
         country.cap_city,
         country.population,
         country.gdp_in_usd_T,
-        country.presedent,
         sep=" | "
         )
+
 
